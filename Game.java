388325249129ce9ae62443666a371c2b213b5de0
NicholasPaniccioli <nicholaspanic20@gmail.com>
2023-01-08 16:30:56 -0500

    Add More logic to the main game
    In preparation for adding the placing of pieces to the board, added a more thorough branching of choices. Also changed names of options and fields for clarity of what they do.

--- Game.java
+++ Game.java
@@ -4,6 +4,7 @@ class Game {
 
         boolean quitProgram = false;
         boolean quitGame = false;
+        boolean goBack = false;
 
         //To get help get input from the console/player
         InputStreamReader isr = new InputStreamReader(System.in);
@@ -11,8 +12,9 @@ class Game {
         String input = "";
         
         String intro = "Welcome to the Game!\n";
-        String question = "Would you like to PLAY or QUIT: ";
-        String markQuestion = "Pick a Spot to Mark or QUIT: ";
+        String question = "Would you like to PLAY the game or QUIT the program: ";
+        String markQuestion = "Would you like to PLACE a piece, HIT a space, or QUIT the game: ";
+        String hitQuestion = "Pick a Spot to Hit or go BACK: ";
         
         //Beginning of the Program asks for user input
         System.out.println(intro + question);
@@ -33,25 +35,47 @@ class Game {
                 String[][] playerBoard = BoardManager.createBoard();
                 BoardManager.printBoard(playerBoard);
 
-                //Loops so the player can continue to mark until they quit
+                //Loops so the player can continue to play until they quit
                 while(!quitGame){
 
-                    System.out.print("\n" + markQuestion);
+                    //Asks about marking spaces ie PLACE, HIT, BACK
+                    System.out.println(markQuestion);
                     try{
                         input = buffer.readLine().trim().toLowerCase();
                     }catch(IOException e){
                         System.out.println("\nPlease put in a proper action\n" + question);
                     }
 
-                    //Checks if the player wants to quit or mark
-                    //Also checks that input is an acceptable format
-                    if(input.equals("quit")){
+                    if(input.equals("place")){
+                        //start placing pieces
+                    }else if(input.equals("hit")){
+
+                        //Loops until the player decides to stop hitting
+                        while(!goBack){
+                            System.out.print("\n" + hitQuestion);
+                            try{
+                                input = buffer.readLine().trim().toLowerCase();
+                            }catch(IOException e){
+                                System.out.println("\nPlease put in a proper action\n" + question);
+                            }
+        
+                            //Checks if the player wants to go back or hit a space
+                            //Also checks that input is an acceptable format
+                            if(input.equals("back")){
+                                goBack = true;
+                            }else if(input.length() <= 3 && input.length() > 0 && BoardManager.letterToNum(input) != -1){
+                                BoardManager.markBoard(playerBoard, input);
+                                BoardManager.printBoard(playerBoard);
+                            }else{
+                                System.out.println("\nPlease put in an acceptable response\n" + hitQuestion);
+                            }
+                        }
+                        goBack = false;
+
+                    }else if(input.equals("quit")){
                         quitGame = true;
-                    }else if(input.length() <= 3 && input.length() > 0 && BoardManager.letterToNum(input) != -1){
-                        BoardManager.markBoard(playerBoard, input);
-                        BoardManager.printBoard(playerBoard);
                     }else{
-                        System.out.println("\nPlease put in an acceptable response\n" + markQuestion);
+                        System.out.println("\nPlease give a valid action.");
                     }
                 }
                 System.out.println("\nYou played the Game! Thanks for playing!\n" + question);
