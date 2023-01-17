7b03336787d72237cf31b28d730e0dc718e4dfc7
Nicholas Paniccioli <nicholaspanic20@gmail.com>
2023-01-17 17:44:29 -0500

    Space Out Console Text
    Provided more spacing in any printed text to help give better readability and avoid clumps of text.

--- Game.java
+++ Game.java
@@ -13,13 +13,13 @@ class Game {
         BufferedReader buffer = new BufferedReader(isr);
         String input = "";
         
-        String intro = "Welcome to the Game!\n";
-        String question = "Would you like to PLAY the game or QUIT the program: ";
-        String markQuestion = "Would you like to PLACE a piece, HIT a space, or QUIT the game: ";
-        String hitQuestion = "Pick a Spot to Hit or go BACK: ";
+        String intro = "\n Welcome to the Game!\n";
+        String question = " Would you like to PLAY the game or QUIT the program: ";
+        String markQuestion = "\n Would you like to PLACE a piece, HIT a space, or QUIT the game: ";
+        String hitQuestion = " Pick a Spot to Hit or go BACK: ";
         
         //Beginning of the Program asks for user input
-        System.out.println(intro + question);
+        System.out.print(intro + question);
         //Loops until the player quits the program
         while(!quitProgram){
 
@@ -27,13 +27,13 @@ class Game {
             try{
                 input = buffer.readLine().trim().toLowerCase();
             }catch(IOException e){
-                System.out.println("\nPlease put in a proper action\n" + question);
+                System.out.println("\n Please put in a proper action\n" + question);
             }
             
             //Checks input for play or quit, and gives appropriate action
             if(input.equals("play")){
                 
-                System.out.println("\nNew board created:");
+                System.out.println("\n New board created:");
                 String[][] playerBoard = BoardManager.createBoard();
                 BoardManager.printBoard(playerBoard);
 
@@ -41,18 +41,18 @@ class Game {
                 while(!quitGame){
 
                     //Asks about marking spaces ie PLACE, HIT, BACK
-                    System.out.println(markQuestion);
+                    System.out.print(markQuestion);
                     try{
                         input = buffer.readLine().trim().toLowerCase();
                     }catch(IOException e){
-                        System.out.println("\nPlease put in a proper action\n" + markQuestion);
+                        System.out.println("\n Please put in a proper action\n" + markQuestion);
                     }
 
                     if(input.equals("place")){
                         //If they are placing needs to refresh board to make new space.
-                        System.out.println("Reseting board!");
+                        System.out.println("\n Reseting board!");
                         playerBoard = BoardManager.createBoard();
-                        System.out.println("Board was reset!");
+                        System.out.println(" Board was reset!");
                         String startPoint;
                         String endPoint;
                         
@@ -60,21 +60,21 @@ class Game {
                             boolean placed = false;
 
                             while(!placed){
-                                System.out.println("Pick a Starting Point for a piece that's " + pieceLengths[i] + " spaces in length: ");
+                                System.out.print("\n Pick a Starting Point for a piece that's " + pieceLengths[i] + " spaces in length: ");
                                 try{
                                     input = buffer.readLine().trim().toLowerCase();
                                 }catch(IOException e){
-                                    System.out.println("\nPlease put in a proper action\n");
+                                    System.out.println("\n Please put in a proper action\n");
                                 }
                                 
                                 //Checks that the input is a coordinate and then stores it
                                 if(BoardManager.coordinateCheck(input)){
                                     startPoint = input;
-                                    System.out.println("Pick an Ending Point for the piece: ");
+                                    System.out.print("\n Pick an Ending Point for the piece: ");
                                     try{
                                         input = buffer.readLine().trim().toLowerCase();
                                     }catch(IOException e){
-                                        System.out.println("\nPlease put in a proper action\n");
+                                        System.out.println("\n Please put in a proper action\n");
                                     }
     
                                     //Checks that the second input is a coordinate and stores it
@@ -86,15 +86,15 @@ class Game {
                                        placed = BoardManager.placingCheck(playerBoard, startPoint, endPoint, pieceLengths[i]);
     
                                     }else{
-                                        System.out.println("\nPlease put in an acceptable response\n");
+                                        System.out.println("\n Please put in an acceptable response\n");
                                     }
     
                                 }else{
-                                    System.out.println("\nPlease put in an acceptable response\n");
+                                    System.out.println("\n Please put in an acceptable response\n");
                                 }
                             }
                             //Feedback after placing piece
-                            System.out.println("Piece was placed!\n");
+                            System.out.println("\n The piece was placed!\n");
                             BoardManager.printBoard(playerBoard);
                         }
                     }else if(input.equals("hit")){
@@ -105,7 +105,7 @@ class Game {
                             try{
                                 input = buffer.readLine().trim().toLowerCase();
                             }catch(IOException e){
-                                System.out.println("\nPlease put in a proper action\n" + hitQuestion);
+                                System.out.println("\n Please put in a proper action\n" + hitQuestion);
                             }
         
                             //Checks if the player wants to go back or hit a space
@@ -116,7 +116,7 @@ class Game {
                                 BoardManager.hitBoard(playerBoard, input);
                                 BoardManager.printBoard(playerBoard);
                             }else{
-                                System.out.println("\nPlease put in an acceptable response\n");
+                                System.out.println("\n Please put in an acceptable response\n");
                             }
                         }
                         goBack = false;
@@ -124,18 +124,18 @@ class Game {
                     }else if(input.equals("quit")){
                         quitGame = true;
                     }else{
-                        System.out.println("\nPlease give a valid action.");
+                        System.out.println("\n Please give a valid action.");
                     }
                 }
-                System.out.println("\nYou played the Game! Thanks for playing!\n" + question);
+                System.out.print("\n You played the Game! Thanks for playing!\n" + question);
                 quitGame = false;
 
             }else if(input.equals("quit")){
                 quitProgram = true;
             }else{
-                System.out.println("\nPlease give a valid action.\n" + question);
+                System.out.println("\n Please give a valid action.\n" + question);
             }
         }
-        System.out.println("\nYou quit the program");
+        System.out.println("\n You quit the program");
     }
 }
